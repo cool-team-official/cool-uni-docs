f881c03af75f653e081135471e23df0aaed469d3
神仙都没用 <2570063477@qq.com>
2024-07-16 18:31:00 +0800

    优化

--- .vitepress/theme/layout.vue
+++ .vitepress/theme/layout.vue
@@ -1,5 +1,16 @@
 <template>
   <Layout>
+    <template #sidebar-nav-before>
+      <div class="ad">
+        <img
+          v-for="item in ad.list"
+          :key="item"
+          :src="item.pic"
+          @click="ad.toLink(item)"
+        />
+      </div>
+    </template>
+
     <template #aside-outline-before>
       <demo is-fr :path="path" :key="path" />
     </template>
@@ -10,11 +21,36 @@
 import DefaultTheme from "vitepress/theme";
 import Demo from "../../components/demo.vue";
 import { useRoute } from "vitepress";
-import { watch, ref } from "vue";
+import { watch, ref, onMounted, reactive } from "vue";
 
 const route = useRoute();
 
 const { Layout } = DefaultTheme;
+
+const ad = reactive({
+  list: [],
+
+  toLink(item) {
+    if (item.link) {
+      window.open(item.link);
+    } else {
+      window.open(`https://cool-js.com/ad/${item.id}`);
+    }
+  },
+
+  get() {
+    fetch("http://192.168.0.119:9009/app/info/ad/list", {
+      method: "POST",
+      body: {
+        channel: 1,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        this.list = res.data;
+      });
+  },
+});
 
 const path = ref("");
 
@@ -36,4 +72,22 @@ watch(
     deep: true,
   }
 );
+
+onMounted(() => {
+  ad.get();
+});
 </script>
+
+<style scoped>
+.ad img {
+  display: block;
+  width: 100%;
+  margin-top: 10px;
+  border-radius: 8px;
+  cursor: pointer;
+}
+
+.ad img:hover {
+  opacity: 0.9;
+}
+</style>
